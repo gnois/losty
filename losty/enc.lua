90503139d2f2a2070a624813f12afa8c4ebf25a5
unknown <weishng@hotmail.com>
2019-11-07 01:52:32 +0800

    Check function arg for nil

--- losty/enc.lua
+++ losty/enc.lua
@@ -4,20 +4,24 @@
 local json = require("cjson.safe")
 local K = {}
 K.encode = function(obj, func)
-    local str, err = json.encode(obj)
+    if obj then
+        local str, err = json.encode(obj)
+        if str then
+            if func then
+                str = func(str)
+            end
+            return ngx.encode_base64(str)
+        end
+        return str, err
+    end
+end
+K.decode = function(str, func)
     if str then
+        str = ngx.decode_base64(str)
         if func then
             str = func(str)
         end
-        return ngx.encode_base64(str)
+        return json.decode(str)
     end
-    return str, err
-end
-K.decode = function(str, func)
-    str = ngx.decode_base64(str)
-    if func then
-        str = func(str)
-    end
-    return json.decode(str)
 end
 return K
