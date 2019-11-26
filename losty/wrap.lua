819d913aacb2d2dcaaee123db844c4b63d25ac6d
unknown <weishng@hotmail.com>
2019-11-27 00:00:43 +0800

    Fix nil ref

--- losty/wrap.lua
+++ losty/wrap.lua
@@ -3,7 +3,6 @@
 --
 local enc = require("losty.enc")
 local surl = require("losty.surl")
-local hmac = ngx.hmac_sha1
 return function(secret, key)
     if not (secret and key) then
         error("secret and key required", 2)
@@ -11,7 +10,8 @@ return function(secret, key)
     local pen = surl(secret)
     return {wrap = function(data, func, length)
         local obj = {key = key, data = data}
-        return pen.sign(str, length), enc.encode(obj, func)
+        local text = enc.encode(obj, func)
+        return pen.sign(text, length), text
     end, unwrap = function(sig, text, func, length)
         assert(text)
         if pen.verify(sig, text, length) then
