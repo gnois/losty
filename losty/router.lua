c293ddca4461d9e06018bb84e0f2f7cf60bdf4a6
unknown <weishng@hotmail.com>
2019-12-20 18:51:37 +0800

    Router has deterministic match order, accepts lua pattern, but no more named wildcard

--- losty/router.lua
+++ losty/router.lua
@@ -1,73 +1,144 @@
 --
 -- Generated from router.lt
 --
-return function()
-    local COLON = string.byte(":", 1)
-    local LEAF = "~"
+local str_sub = string.sub
+local str_find = string.find
+local str_match = string.match
+local COLON = ":"
+local LEAF = "#"
+local router = function()
     local tree = {}
-    local K = {}
     local resolve
-    resolve = function(path, node, params)
+    resolve = function(path, nodes, matches, m)
         local _, token
-        _, _, token, path = string.find(path, "([^/]+)(.*)")
+        _, _, token, path = str_find(path, "([^/]+)(.*)")
         if not token then
-            return node[LEAF], params
+            return nodes[LEAF], matches
         end
-        for child_token, child_node in pairs(node) do
-            if child_token == token then
-                local func, bindings = resolve(path, child_node, params)
+        local child = nodes[token]
+        if child then
+            local func, bindings = resolve(path, child, matches, m)
+            if func then
+                return func, bindings
+            end
+        end
+        local pattern
+        local toklen = #token
+        for __, node in ipairs(nodes) do
+            pattern, child = next(node)
+            local prev = m
+            if (function(s, e, ...)
+                if s == 1 and e == toklen then
+                    matches[m] = token
+                    m = m + 1
+                    for ___, v in ipairs({...}) do
+                        matches[m] = v
+                        m = m + 1
+                    end
+                    return true
+                end
+            end)(str_find(token, pattern)) then
+                local func, bindings = resolve(path, child, matches, m)
                 if func then
                     return func, bindings
                 end
             end
-        end
-        for child_token, child_node in pairs(node) do
-            if string.byte(child_token, 1) == COLON then
-                local name = string.sub(child_token, 2)
-                local value = params[name]
-                params[name] = token or value
-                local func, bindings = resolve(path, child_node, params)
-                if func then
-                    return func, bindings
-                end
-                params[name] = value
-            end
+            m = prev
         end
         return false
     end
-    local install = function(node, path, ...)
-        for token in string.gmatch(path, "[^/]+") do
-            node[token] = node[token] or {}
-            node = node[token]
+    local check = function(pattern)
+        if not str_find(pattern, "[%.%%]") and not str_find(pattern, "%b[]") then
+            return false, "'" .. pattern .. "' does not have pattern"
         end
-        local old = node[LEAF]
-        if nil == old then
-            node[LEAF] = {...}
-        else
-            for _, f in ipairs({...}) do
-                old[#old + 1] = f
+        if str_find(pattern, "[:#]") then
+            return false, "'" .. pattern .. "' may not match any url"
+        end
+        for _, c in ipairs({"%%c", "%%s"}) do
+            if str_find(pattern, c) then
+                return false, "'" .. str_sub(c, 2) .. "' may not match any url"
             end
-            node[LEAF] = old
+        end
+        return pcall(str_find, pattern, pattern)
+    end
+    local find_create = function(nodes, token)
+        local x
+        for _, n in ipairs(nodes) do
+            x = n[token]
+            if x then
+                break
+            end
+        end
+        if not x then
+            x = {}
+            table.insert(nodes, {[token] = x})
+        end
+        return x
+    end
+    local install = function(nodes, path, ...)
+        for token in string.gmatch(path, "[^/]+") do
+            if COLON == str_sub(token, 1, 1) then
+                if str_match(token, ":%b()") == token then
+                    token = str_sub(token, 3, -2)
+                else
+                    token = str_sub(token, 2)
+                end
+                local ok, err = check(token)
+                if not ok then
+                    error(err .. " in " .. path)
+                end
+                nodes = find_create(nodes, token)
+            else
+                nodes[token] = nodes[token] or {}
+                nodes = nodes[token]
+            end
+        end
+        local old = nodes[LEAF]
+        if not old then
+            old = {...}
+        else
+            local o = #old
+            for n, f in ipairs({...}) do
+                old[o + n] = f
+            end
+        end
+        nodes[LEAF] = old
+    end
+    local invalid = function(path)
+        if not path or #path < 1 then
+            return "is empty"
+        end
+        if not str_sub(path, 1, 1) == "/" then
+            return "does not start with '/'"
+        end
+        if str_find(path, "%s") then
+            return "has space"
+        end
+        if str_find(path, "//") then
+            return "has empty segment"
         end
     end
-    K.match = function(method, path)
-        local node = tree[method]
-        if not node then
-            return nil, string.format("Unknown method: %s", method)
+    return {match = function(method, path)
+        local nodes = tree[method]
+        if not nodes then
+            return nil, "unmatched method: " .. (method or "")
         end
         path = string.gsub(path, "%?.*", "")
-        local func, params = resolve(path, node, {})
-        if not func then
-            return nil, string.format("Could not resolve %s %s", method, path)
+        local arr, matches = resolve(path, nodes, {}, 1)
+        if not arr then
+            return nil, "unmatched path: " .. (path or "")
         end
-        return func, params
-    end
-    K.set = function(method, path, ...)
-        assert(path and #path > 0)
+        return arr, matches
+    end, set = function(method, path, ...)
+        local err = invalid(path)
+        if err then
+            error("route '" .. path .. "' " .. err)
+        end
         if not tree[method] then
             tree[method] = {}
         end
         install(tree[method], path, ...)
-    end
-    return K
+        return tree[method]
+    end}
 end
+return router
