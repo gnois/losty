9fb892e8858779fca45f6d95f7cb3f323dfbd020
unknown <weishng@hotmail.com>
2020-08-07 13:24:04 +0800

    Update idempotent.lt comment. Update locked.lt

--- losty/locked.lua
+++ losty/locked.lua
@@ -1,29 +1,22 @@
 --
 -- Generated from locked.lt
 --
-local rlock = require("resty.lock")
-return function(lock, key, done, create, ...)
-    local val, err = done(key)
+local locker = require("resty.lock")
+return function(lock_name, key, expiry, read, write, ...)
+    local val, err = read(key)
     if val ~= nil then
         return val
     end
-    local locker = rlock:new(lock)
-    local elapsed
-    elapsed, err = locker:lock(key)
-    if not elapsed then
-        ngx.log(ngx.ERR, "Fail to acquire '", key, "' from ", lock, ": ", err)
-        return nil, err
-    end
-    ngx.log(ngx.INFO, "Acquired '", key, "' from ", lock, " after ", elapsed, " seconds")
-    val, err = done(key)
-    if val == nil then
-        val, err = create(...)
-    end
+    local lock = locker:new(lock_name, {exptime = expiry})
     local ok
-    ok, err = locker:unlock()
+    ok, err = lock:lock(key)
     if not ok then
-        ngx.log(ngx.ERR, "Fail to release '", key, "' from ", lock, ": ", err)
         return nil, err
     end
+    val, err = read(key)
+    if val == nil then
+        val, err = write(...)
+    end
+    ok, err = lock:unlock()
     return val, err
 end
