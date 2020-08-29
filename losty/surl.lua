7ffea6fd6dc03a25056f0be62e02e698bcde7d74
unknown <weishng@hotmail.com>
2020-08-29 13:18:29 +0800

    lift length parameter to constructor so wrap and unwrap checks the same length

--- losty/surl.lua
+++ losty/surl.lua
@@ -3,24 +3,25 @@
 --
 local enc = require("losty.enc")
 local hmac = ngx.hmac_sha1
-return function(secret)
+return function(secret, length)
     if not secret then
         error("secret required", 2)
     end
-    return {sign = function(payload, length)
+    if length then
+        assert(length > 1, "signed url length must be greater than 1")
+    end
+    return {sign = function(payload)
         assert(payload)
         local sig = enc.encode64(hmac(secret, payload))
         if length then
-            assert(length > 1)
             return string.sub(sig, 1, length)
         end
         return sig
-    end, verify = function(sig, payload, length)
+    end, verify = function(sig, payload)
         assert(payload)
         if sig then
             local mac = enc.encode64(hmac(secret, payload))
             if length then
-                assert(length > 1)
                 return string.sub(sig, 1, length) == string.sub(mac, 1, length)
             end
             return sig == mac
--- losty/wrap.lua
+++ losty/wrap.lua
@@ -3,18 +3,18 @@
 --
 local enc = require("losty.enc")
 local surl = require("losty.surl")
-return function(secret, key)
+return function(secret, key, length)
     if not (secret and key) then
         error("secret and key required", 2)
     end
-    local pen = surl(secret)
-    return {wrap = function(data, func, length)
+    local pen = surl(secret, length)
+    return {wrap = function(data, func)
         local obj = {key = key, data = data}
         local text = enc.encode(obj, func)
-        return pen.sign(text, length), text
-    end, unwrap = function(sig, text, func, length)
+        return pen.sign(text), text
+    end, unwrap = function(sig, text, func)
         assert(text)
-        if pen.verify(sig, text, length) then
+        if pen.verify(sig, text) then
             local obj = enc.decode(text, func)
             if obj.key == key then
                 return obj.data
