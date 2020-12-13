b210d49a88e5b570003fd7f163738ced30b9fceb
unknown <weishng@hotmail.com>
2020-12-13 23:03:16 +0800

    Simplify code

--- losty/locked.lua
+++ losty/locked.lua
@@ -2,21 +2,20 @@
 -- Generated from locked.lt
 --
 local locker = require("resty.lock")
-return function(lock_name, key, expiry, read, write, ...)
+local f = function(lock_name, key, expiry, read, write, ...)
     local val, err = read(key)
-    if val ~= nil then
-        return val
-    end
-    local lock = locker:new(lock_name, {exptime = expiry})
-    local ok
-    ok, err = lock:lock(key)
-    if not ok then
-        return nil, err
-    end
-    val, err = read(key)
     if val == nil then
-        val, err = write(key, ...)
+        local lock = locker:new(lock_name, {exptime = expiry})
+        local ok
+        ok, err = lock:lock(key)
+        if ok then
+            val, err = read(key)
+            if val == nil then
+                val, err = write(key, ...)
+            end
+            lock:unlock()
+        end
     end
-    ok, err = lock:unlock()
     return val, err
 end
+return f
