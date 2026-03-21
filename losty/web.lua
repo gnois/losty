aac0f5a4b9950d1a93c018fda6b19637a7aff2c2
gnois <weishng@hotmail.com>
2026-03-21 17:42:40 +0800

    Simplify to one router per worker. Enforce route registration in init_by_lua_block for performance reason

--- losty/web.lua
+++ losty/web.lua
@@ -14,68 +14,70 @@ local METHODS = {
     , "PATCH"
     , "OPTIONS"
 }
-return function()
-    local rt = router()
-    local route = function(prefix)
-        local r = {}
-        for _, meth in ipairs(METHODS) do
-            r[string.lower(meth)] = function(path, f, ...)
-                if prefix and prefix ~= "/" then
-                    path = prefix .. path
-                end
-                rt.set(meth, path, f, ...)
-            end
-        end
-        return r
+local rt = router()
+local route = function(prefix)
+    local phase = ngx.get_phase()
+    if phase ~= "init" then
+        error("route() must be called in init_by_lua_block instead of '" .. phase .. "' phase", 2)
     end
-    local run = function(error_page, check)
-        local handlers, body, ok, trace
-        local q = req()
-        local r = res()
-        local method = q.vars.request_method
-        handlers, q.match = rt.match(method == "HEAD" and "GET" or method, q.vars.uri)
-        if handlers then
-            ok, trace = xpcall(function()
-                body = dispatch(handlers, q, r)
-            end, function(err)
-                return debug.traceback(err, 2)
-            end)
-            if not ok then
-                r.status = 500
-                ngx.log(ngx.ERR, trace)
+    local r = {}
+    for _, meth in ipairs(METHODS) do
+        r[string.lower(meth)] = function(path, f, ...)
+            if prefix and prefix ~= "/" then
+                path = prefix .. path
             end
-        else
-            r.status = 404
+            rt.set(meth, path, f, ...)
         end
-        local code = r.status
-        if error_page == true and body == nil and code >= 400 then
-            ngx.exit(code)
-            return code, trace
-        end
-        if check then
-            if code == 0 then
-                error("Response status required")
-            end
-            if body ~= nil or code >= 200 and code < 300 then
-                if not statuses.is_empty(code) and r.headers["Content-Type"] == nil then
-                    error("Content-Type header required")
-                end
-            end
-        end
-        local err
-        ok, err = r.send()
-        if ok then
-            if method ~= "HEAD" and not statuses.is_empty(code) and body ~= nil then
-                ok, err = ngx.print(body)
-            end
-            if ok then
-                ok, err = ngx.eof()
-            end
-        end
-        if not ok then
-            ngx.log(ngx.ERR, err)
-        end
-        return code, err or trace
     end
-    return {route = route, run = run}
+    return r
 end
+local run = function(error_page, check)
+    local handlers, body, ok, trace
+    local q = req()
+    local r = res()
+    local method = q.vars.request_method
+    handlers, q.match = rt.match(method == "HEAD" and "GET" or method, q.vars.uri)
+    if handlers then
+        ok, trace = xpcall(function()
+            body = dispatch(handlers, q, r)
+        end, function(err)
+            return debug.traceback(err, 2)
+        end)
+        if not ok then
+            r.status = 500
+            ngx.log(ngx.ERR, trace)
+        end
+    else
+        r.status = 404
+    end
+    local code = r.status
+    if error_page == true and body == nil and code >= 400 then
+        return ngx.exit(code)
+    end
+    local empty = statuses.is_empty(code)
+    if check then
+        if code == 0 then
+            error("Response status required")
+        end
+        if body ~= nil or code >= 200 and code < 300 then
+            if not empty and r.headers["Content-Type"] == nil then
+                error("Content-Type header required")
+            end
+        end
+    end
+    local err
+    ok, err = r.send()
+    if ok then
+        if method ~= "HEAD" and not empty and body ~= nil then
+            ok, err = ngx.print(body)
+        end
+        if ok then
+            ok, err = ngx.eof()
+        end
+    end
+    if not ok then
+        ngx.log(ngx.ERR, err)
+    end
+    return code, err or trace
+end
+return {route = route, run = run}
